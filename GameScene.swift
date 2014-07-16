8670e6b94af5339d5ddbc238ac3ca68adfed3819
qq251569880 <qq251569880@gmail.com>
2014-07-16 09:41:09 +0800

    代码整理
    整理代码，添加注释

--- GameScene.swift
+++ GameScene.swift
@@ -11,42 +11,55 @@ import Foundation
 import UIKit
 
 class GameScene: SKScene,DiceGameDelegate {
+
+    //记录游戏背景图片的中心位置，就是蛇和梯子的25格图
     var boardP:CGPoint = CGPointMake(0,0)
+    //记录游戏背景图片的尺寸大小
     var boardSize:CGSize = CGSizeMake(0,0)
-   
+
+    //游戏背景图片元素
+    var spriteBoard = SKSpriteNode()
+    //飞船元素，在此Demo中以项目已经给的默认图片飞船为在方格上移动的玩家。
     var spriteShip = SKSpriteNode()
 
-    var spriteBoard = SKSpriteNode()
-    var moveForward = SKAction()
-    var moveBack = SKAction()
-    var moveUp = SKAction()
-    var moveRotateRight = SKAction()
-    var moveRotateLeft = SKAction()
+    struct PointProperty
+    {
+        var pointPosition:CGPoint = CGPointMake(0,0)
+        var angle:CGFloat = 0.0
+    }
+    var pointsPosition:Dictionary<Int,PointProperty> = [:]
+    
+    var stepOn = 1
+    var myLabel = SKLabelNode()
+    //定义一个游戏实例
     let game = SnakesAndLadders()
-    
+    //记录掷色子的次数，即投掷了多少次色子获得胜利
+    var numberOfTurns = 0
 
-    
     override func didMoveToView(view: SKView) {
         println("present scane")
         /* Setup your scene here */
-        //背景颜色
+        ////设置背景颜色
         var skyColor = SKColor()
         skyColor = SKColor(red: 81.0/255.0, green: 192.0/255.0, blue: 201.0/255.0, alpha: 1.0)
         self.backgroundColor = skyColor
-        //背景图片，游戏图表
+        //添加背景图片，蛇和梯子的25格图
         var skyTexture = SKTexture(imageNamed: "board")
-        
+        //设置大小和位置
         spriteBoard = SKSpriteNode(texture:skyTexture)
+        //它的位置是左右居中。Scene的Y轴是由下至上的，所以是屏幕高度除以2+150，即中间靠上的位置
         spriteBoard.position = CGPointMake(self.frame.size.width/2,spriteBoard.size.height/2+150)
         boardP = spriteBoard.position
+        //在模拟器中我使用screen的大小为参考，否则很难控制这个图片的显示。
         var screen = UIScreen.mainScreen()
         println("screen width:\(screen.bounds.size.width),height:\(screen.bounds.size.height)")
         println("board width:\(spriteBoard.size.width),height:\(spriteBoard.size.height)")
 
         spriteBoard.setScale(screen.bounds.size.width/spriteBoard.size.width*1.3)
         boardSize = spriteBoard.size
-        //上部文字说明
         self.addChild(spriteBoard)
+        
+        //添加标签，上部文字说明
         myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "player one";
         myLabel.fontSize = 20;
@@ -55,40 +68,49 @@ class GameScene: SKScene,DiceGameDelegate {
         self.addChild(myLabel)
 
         
-        //飞机
+        //用飞船代替玩家
         spriteShip = SKSpriteNode(imageNamed:"Spaceship")
         println("spaceShip width:\(spriteShip.size.width),height:\(spriteShip.size.height)")
         spriteShip.setScale(screen.bounds.size.width/boardSize.width*0.2)
+        //它的初始位置是25格的最左最下的方格。计算方法是以25格图中心点为参考，向左移动两格，向下移动两格，即第一个方格。
         spriteShip.position = CGPointMake(boardP.x-boardSize.width/5*2,spriteBoard.position.y-boardSize.height/5*2)
         self.addChild(spriteShip)
-        
+        //给game的delegate赋值并开始游戏
         game.delegate = self
         game.start()
-        //设置动画
+        //添加游戏的代理，并设置飞船在每一个点的位置和朝向
         var distanceX = boardSize.width/5
-        
         var distanceY = boardSize.height / 5
+        //以第一个点为参考坐标
         let originPos:CGPoint = spriteShip.position
         for i:Int in 0..25 {
+            //cy是行数,Y轴方向
             var cy = i/5
             var mark:Int = 1
+            //第0行是正向，第1行是反方向
             if(cy%2 != 0){
                 mark = -1
             }
-
+            //对着图看，X是从0加到4，又从4减到0，所以根据行数来决定是加还是减
+            //	1	 	2		3		4		5		6		7		8		9		10 		...
+            //(0,0)		(1,0)	(2,0)	(3,0)	(4,0)	(4,1)	(3,1)	(2,1)	(1,1)	(0,1)
             var cx = (i%5)*mark + ((i/5)%2)*4;
             //println("\(i):(\(cx),\(cy))")
             var px = originPos.x + CGFloat(cx) * distanceX
             var py = originPos.y + CGFloat(cy) * distanceY
             var pos = CGPointMake(px,py)
+            //方向
             var angle:CGFloat = M_PI;
             if((i+1)%5 == 0){
+                //第5，10,15,20,25格式朝上的，角度为90
                 angle = M_PI / 2.0
             }
             else{
                 if((i/5) % 2 == 0){
+                    //第0,2,4行是朝右的，角度为0
                     angle = 0.0
                 }else{
+                    //第1,3行是朝左的，角度为180
                     angle = M_PI
                 }
             }
@@ -105,60 +127,13 @@ class GameScene: SKScene,DiceGameDelegate {
             if node === spriteBoard {
                 game.play();
             }
-            /*            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)*/
-/*            if(stepOn == 25){
-                continue
-            }
-            
-            //让飞机移动
-            if(stepOn%5 == 0){
-                if(stepOn/5%2 == 0){
-                    spriteShip.runAction(moveUpRight)
-                }
-                else{
-                    spriteShip.runAction(moveUpLeft)
-                }
-            }
-            else
-            {
-                if(stepOn/5%2 == 0){
-                    spriteShip.runAction(moveForward)
-                }
-                else{
-                    spriteShip.runAction(moveBack)
-                }
-            }
-            stepOn++;
-            if(stepOn == 25){
-                myLabel.text = "You Win,Game Over!"
-            }
-*/
         }
     }
     
-    struct PointProperty
-    {
-        var pointPosition:CGPoint = CGPointMake(0,0)
-        var angle:CGFloat = 0.0
-    }
-    var pointsPosition:Dictionary<Int,PointProperty> = [:]
-    
-    var stepOn = 1
-    var myLabel = SKLabelNode()
-    
+    //直线前进，从start直接启动到step
     func straightMove(start:Int,end:Int) -> SKAction {
         var actionList:SKAction[] = []
-        
+        //取得移动的x,y长度，求出斜边的长度，通过反cos函数，求得角度（画图研究下吧，别说上学学的东西都用不到了，我是真费了半天的劲）
         var x = pointsPosition[end]!.pointPosition.x - pointsPosition[start]!.pointPosition.x
         
         var y = pointsPosition[end]!.pointPosition.y - pointsPosition[start]!.pointPosition.y
@@ -172,7 +147,7 @@ class GameScene: SKScene,DiceGameDelegate {
             
         }
         
-        
+        //总是旋转比较小的角度
         func rotatePi(direct:CGFloat) -> CGFloat{
             if(direct > M_PI){
                 return direct - 2 * M_PI
@@ -188,11 +163,11 @@ class GameScene: SKScene,DiceGameDelegate {
         rotateAngle1 = rotatePi(rotateAngle1)
         var rotateAngle2 = pointsPosition[end]!.angle - direction
         rotateAngle2 = rotatePi(rotateAngle2)
-        
+        //下面就是动画序列，旋转-》移动-》旋转
         actionList += SKAction.rotateByAngle(rotateAngle1,duration:NSTimeInterval(0.2*fabs(rotateAngle1)))
         actionList += SKAction.moveByX(x,y:y,duration:NSTimeInterval(0.01*distance))
         actionList += SKAction.rotateByAngle(rotateAngle2,duration:NSTimeInterval(0.2*fabs(rotateAngle2)))
-        
+        //本函数仅生成动画序列，然后返回
         return SKAction.sequence(actionList)
     }
     
@@ -217,12 +192,13 @@ class GameScene: SKScene,DiceGameDelegate {
     
     
     
-    var numberOfTurns = 0
+
     func gameDidStart(game:DiceGame) {
         numberOfTurns = 0
         if game is SnakesAndLadders{
             myLabel.text = "Game Is Going"
             let ourGame = game as SnakesAndLadders
+            //假如是游戏进行到一半，重新开始，则飞机从当前移动到起点
             if ourGame.square != 1 {
                 var actions:SKAction[] = []
                 actions += straightMove(ourGame.square,end:1)
@@ -232,10 +208,12 @@ class GameScene: SKScene,DiceGameDelegate {
             
         }
     }
+    //代理的作用是可以直接使用GameScene的元素，比如myLabel,最简单的体现
     func gameDidEnd(game:DiceGame){
         myLabel.text = "You Win Game for \(numberOfTurns) turns"
         
     }
+    //掷色子后的飞船移动
     func gamePlay(diceGame:DiceGame,didStartNewTurnWithDiceRoll diceRoll:Int) -> Int {
         myLabel.text = "You Rolled \(diceRoll)"
         let game = diceGame as SnakesAndLadders
@@ -244,6 +222,7 @@ class GameScene: SKScene,DiceGameDelegate {
         var steps:Int = 0
         var currentSquare = game.square
         var actions:SKAction[] = []
+        //这是对游戏的一个小改善，如果没有正好到达25格，则后退，直到正好抵达25格算是游戏结束
         if(currentSquare + diceRoll > game.finalSquare){
             steps = game.finalSquare - currentSquare
             actions += diceStepForward(currentSquare,step:steps)
@@ -259,11 +238,13 @@ class GameScene: SKScene,DiceGameDelegate {
             steps = diceRoll
             currentSquare += steps
         }
+        //这是遇到蛇或者梯子了，直接后退或者前进
         if(game.board[currentSquare] != 0){
             actions += straightMove(currentSquare,end:(currentSquare+game.board[currentSquare]))
             println("strait go from \(currentSquare) to \(currentSquare+game.board[currentSquare])")
             steps += game.board[currentSquare]
         }
+        //调用飞船的runAction，执行动画播放
         spriteShip.runAction(SKAction.sequence(actions))
         return steps
     }
--- Protocol.swift
+++ Protocol.swift
@@ -10,32 +10,33 @@ import Foundation
 import SpriteKit
 
 let ARC4RANDOM_MAX:Double = 0x100000000
-
+//产生随机数的协议
 protocol RandomNumberGenerator{
     func random()-> Double
 }
-
+//色子游戏的协议
 protocol DiceGame{
     var dice : Dice {get}
     func play()
 }
-
+//色子游戏代理的协议
 protocol DiceGameDelegate {
     func gameDidStart(game:DiceGame)
     func gamePlay(game:DiceGame,didStartNewTurnWithDiceRoll diceRoll:Int) -> Int
     func gameDidEnd(game:DiceGame)
     func gameAlert()
 }
-
+//产生随机数的类
 class GenerateRandom:RandomNumberGenerator{
     func random()-> Double{
         return Double(arc4random())/ARC4RANDOM_MAX
     }
 }
-
+//色子类
 class Dice {
+    //色子面数，由此决定roll的点数的范围
     let sides:Int
-    
+    //产生随机数生成器，类型是RandomNumberGenerator，用实现了该协议的类实例初始化
     let generator:RandomNumberGenerator
     
     
@@ -49,17 +50,17 @@ class Dice {
         
     }
 }
-
+//真正的游戏类
 class SnakesAndLadders:DiceGame {
-    
+    //这个代理可以理解为：这个代理将具体实现这个游戏的操作，比如游戏状态的显示，飞机的移动等。我认为这个代理应该是GameScene类
     var delegate : DiceGameDelegate?
     
     let finalSquare = 25
-    
+    //色子初始化为6面的，随机数产生器使用的是GenerateRandom的对象
     let dice = Dice(sides:6,generator:GenerateRandom())
     
     var square = 1
-    
+     //记录蛇和梯子的起点和跳跃的步数
     var board:Int[]
     
     var gameOver = false
@@ -69,15 +70,19 @@ class SnakesAndLadders:DiceGame {
         board[03] = +08;board[06] = +11;board[09] = +09;board[10] = +02;
         board[14] = -10;board[19] = -11;board[22] = -02;board[24] = -08;
     }
+    //我添加了一个convenience构造函数，助于理解convenience构造函数理解可以设置格子数量（本Demo是用不到了）
     convenience init(finalSquare:Int) {
         self.init()
         self.finalSquare = finalSquare
     }
+    //我将开始的过程单独实现一次的原因是不至于让这个游戏只能玩一次，可以重新开始
     func start(){
         delegate?.gameDidStart(self)
         square = 1
     }
+    //本函数调用一次，就会掷色子一次，飞机就会前进一次
     func play(){
+        //如果已经到了终点，提示是否重新开始
         if (square == finalSquare){
             println("ask restart?")
             delegate?.gameAlert()
@@ -87,6 +92,7 @@ class SnakesAndLadders:DiceGame {
         else {
             var diceRoll = dice.roll()
             var steps = delegate?.gamePlay(self,didStartNewTurnWithDiceRoll:diceRoll)
+            //判断是否已经到了终点，如果到了终点，则调用结束游戏方法，以显示游戏状态
             if (square+steps! == finalSquare){
                 end()
             }
